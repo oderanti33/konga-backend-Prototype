87056fdef2959b27c2d19b4d3961ee853016f3cf
oderanti33 <49322996+oderanti33@users.noreply.github.com>
2023-10-26 02:00:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader( Access-Control-Allow-Credentials: true);
+  res.setHeader( 'Access-Control-Allow-Credentials': true);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
 });
