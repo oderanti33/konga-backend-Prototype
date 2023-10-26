33d92bee525d08855f67a20e5c89e111331d8102
oderanti33 <49322996+oderanti33@users.noreply.github.com>
2023-10-26 16:46:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,17 +14,6 @@ const usersRoutes = require('./Route/users-routes');
 
 const server = express();
 
-server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTION');
-
-  next();
-});
-
 server.use('/api/products', productsRoutes);
 server.use('/api/users', usersRoutes);
 
