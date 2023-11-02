33a2784d06af767f220b03481050280d333f1ce8
oderanti33 <49322996+oderanti33@users.noreply.github.com>
2023-11-02 20:23:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = express();
 server.use(bodyParser.json());
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
