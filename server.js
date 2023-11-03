9a437e2610b97fb75a5601e443b63e941ea9f0e9
oderanti33 <49322996+oderanti33@users.noreply.github.com>
2023-11-03 10:38:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ server.use((req, res, next) => {
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200); 
+    } else {
+        next();
+    }
     next();
 });
 
