a3fcbe8c99111467a930349b3dfe466f3dd8b6bd
oderanti33 <49322996+oderanti33@users.noreply.github.com>
2024-03-20 15:58:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,10 +28,18 @@ const usersRoutes = require('./Route/users-routes');
 //     next();
 // });
 
+const allowedOrigins = ['https://konga-backend.onrender.com/', 'https://konga-backend.onrender.com/api/products', 'http://localhost:3000/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS okay'));
+    }
+  }
+};
 
-server.use(cors({
-  origin: '*'
-}));
+server.use(cors(corsOptions));
 
 server.use('/api/products', productsRoutes);
 server.use('/api/users', usersRoutes);
